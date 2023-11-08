3bf731acd58ab9040b080471f24602e7ee2c6040
skbasha0006 <80062022+skbasha0006@users.noreply.github.com>
2023-11-08 07:50:56 +0000

    Datatypes and conversions

---
+++ 01_Basics/conversionsdatatypes.js
@@ -0,0 +1,20 @@
+let numbers = '333_43';
+/*console.log(numbers);
+console.log(typeof numbers);*/
+let num = Number(numbers)
+console.log(num);
+console.log(typeof num);
+let isLoggedIn = 44;
+//console.log( typeof isLoggedIn);
+let isLog = Boolean(isLoggedIn);
+console.log(isLog);
+console.log(typeof isLog);
+
+let value = 4;
+let negValue = -value;
+console.log(negValue);
+console.log('1' +2+2);
+console.log(2+2+'1');
+console.log(+true);
+console.log(+'');
+ 
---
+++ 01_Basics/datatypes.js
@@ -0,0 +1,23 @@
+/* Datatype : What type/value that can be stored in variable name
+
+*/ 
+let x=3;
+console.log(x);
+let Firstname= 'Mastan';
+console.log(Firstname);
+let isLoggedIn = true;
+console.log(isLoggedIn);
+let isNamed;
+console.log(isNamed);
+let y = null;
+console.log(y);
+let arr = ['bas','sha'];
+console.log(arr);
+
+console.log(typeof(x));
+console.log(typeof(Firstname));
+console.log(typeof(isLoggedIn));
+console.log(typeof(isNamed));
+console.log(typeof(y));
+console.log(typeof(arr));
+console.log(typeof {name:'John', age:34});
